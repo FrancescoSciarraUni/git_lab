b93b3d4f067a17ab94cd1e6b73912e293c797e1e
FrancescoSciarraUni <sciarra.1619797@studenti.uniroma1.it>
2021-09-27 15:40:10 +0200

    commit sul nuovo branch

--- code/exercise_1.py
+++ code/exercise_1.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 if __name__ == "__main__":
     print("hello world")
+    df = pd.read_csv('../git_test.csv')
+    df['Food'] = ['P','G','F','A']
+    df.to_csv('../git_test.csv')
